2dbb09a2df1c353d3a3197e9c284fd0df7e61158
mani4malar <mani4malar@gmail.com>
2022-12-25 16:46:26 +0530

    Initial commit

---
+++ ormtemplate/settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Place with your password generated in Step 1',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'standalone',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
